1f683f6e65660a489265a5f46961856d5f089f8d
José Gustavo Oliveira da Silva <gustavo.myw@live.com>
2022-01-25 12:45:18 -0300

    @Gstv-web [TASK: CORREÇÃO DE BUGS E TESTE NO POSTMAN]

--- lojaGames/src/main/java/com/eCommerce/lojaGames/model/Categoria.java
+++ lojaGames/src/main/java/com/eCommerce/lojaGames/model/Categoria.java
@@ -1,11 +1,17 @@
 package com.eCommerce.lojaGames.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -24,6 +30,9 @@ public class Categoria {
 	@NotBlank
 	private String console;
 
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public long getId() {
 		return this.id;
