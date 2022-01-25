ab6c851b9985761ff18f0816abc27dd0d393ab0a
Jessica Curti <jessica.m.curti@gmail.com>
2022-01-25 11:42:58 -0300

    @Jess-Curti [Correção na categoria - OneToMany]

--- lojaGames/src/main/java/com/eCommerce/lojaGames/model/Categoria.java
+++ lojaGames/src/main/java/com/eCommerce/lojaGames/model/Categoria.java
@@ -1,11 +1,18 @@
 package com.eCommerce.lojaGames.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_categoria")
@@ -23,7 +30,10 @@ public class Categoria {
 	
 	@NotBlank
 	private String console;
-
+	
+	@OneToMany(mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produtos = new ArrayList<>();
 
 	public long getId() {
 		return this.id;
